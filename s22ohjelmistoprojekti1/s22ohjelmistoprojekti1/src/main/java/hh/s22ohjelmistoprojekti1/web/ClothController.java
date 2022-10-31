cb81691edb15a6618281715d7a931be08a8393b9
eliaani <eliashelin98@gmail.com>
2022-10-31 09:56:13 +0200

    controller

--- s22ohjelmistoprojekti1/s22ohjelmistoprojekti1/src/main/java/hh/s22ohjelmistoprojekti1/web/ClothController.java
+++ s22ohjelmistoprojekti1/s22ohjelmistoprojekti1/src/main/java/hh/s22ohjelmistoprojekti1/web/ClothController.java
@@ -1,5 +1,16 @@
 package hh.s22ohjelmistoprojekti1.web;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class ClothController {
 
+	@GetMapping("clothelist")
+	public String showclothers(Model model) {
+		model.addAttribute("Books", clothRepository.findAll());
+		return "clothelist";
+	}
+	
 }
