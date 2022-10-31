953aea5d312a45f9fe2fa9e2704862687164b804
eliaani <eliashelin98@gmail.com>
2022-10-31 10:02:58 +0200

    controller updated

--- s22ohjelmistoprojekti1/s22ohjelmistoprojekti1/src/main/java/hh/s22ohjelmistoprojekti1/web/ClothController.java
+++ s22ohjelmistoprojekti1/s22ohjelmistoprojekti1/src/main/java/hh/s22ohjelmistoprojekti1/web/ClothController.java
@@ -1,16 +1,54 @@
 package hh.s22ohjelmistoprojekti1.web;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import hh.s22ohjelmistoprojekti1.domain.Cloth;
+
+
 
 @Controller
 public class ClothController {
+	
+	@Autowired
+	ClothRepository clothRepository;
 
-	@GetMapping("clothelist")
+	@GetMapping("clothlist")
 	public String showclothers(Model model) {
 		model.addAttribute("Books", clothRepository.findAll());
-		return "clothelist";
+		return "clothlist";
 	}
 	
+    @PreAuthorize("hasAuthority('ADMIN')")
+	@GetMapping("add")
+    public String addCloth(Model model){
+    	model.addAttribute("cloth", new Cloth());
+    	model.addAttribute("categories",clothRepository.findAll());
+        return "addcloth";
+    }
+	
+    @PostMapping("save")
+    public String save(Cloth cloth){
+        clothRepository.save(cloth);
+        return "redirect:clothlist";
+    }
+    
+    @PreAuthorize("hasAuthority('ADMIN')")
+    @GetMapping("/delete/{id}")
+    public String deleteCloth(@PathVariable("id") Long id, Model model) {
+    	clothRepository.deleteById(id);
+        return "redirect:../clothlist";
+    }
+    
+    @PreAuthorize("hasAuthority('ADMIN')")
+    @GetMapping("/edit/{id}")
+    public String editCloth(@PathVariable("id") Long id, Model model) {
+    	model.addAttribute("cloth", clothRepository.findById(id));
+        return "editcloth";
+    }   
 }
