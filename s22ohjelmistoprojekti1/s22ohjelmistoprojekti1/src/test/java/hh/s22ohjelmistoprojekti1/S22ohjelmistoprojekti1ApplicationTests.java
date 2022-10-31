7caa3cd9e60cef5e2321e839160251cbb22c1b89
Tatu Paukkeri <tatu.paukkeri@gmail.com>
2022-10-31 09:26:54 +0200

    first commit

---
+++ s22ohjelmistoprojekti1/s22ohjelmistoprojekti1/src/test/java/hh/s22ohjelmistoprojekti1/S22ohjelmistoprojekti1ApplicationTests.java
@@ -0,0 +1,13 @@
+package hh.s22ohjelmistoprojekti1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class S22ohjelmistoprojekti1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
