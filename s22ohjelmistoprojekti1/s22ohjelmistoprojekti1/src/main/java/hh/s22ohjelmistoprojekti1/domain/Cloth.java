a1bf476c35199622ed6138ce8acc13aacc907642
Teemu Paananen <teemu-paananen@hotmail.com>
2022-10-31 09:56:12 +0200

    cloth.java added

--- s22ohjelmistoprojekti1/s22ohjelmistoprojekti1/src/main/java/hh/s22ohjelmistoprojekti1/domain/Cloth.java
+++ s22ohjelmistoprojekti1/s22ohjelmistoprojekti1/src/main/java/hh/s22ohjelmistoprojekti1/domain/Cloth.java
@@ -1,5 +1,69 @@
 package hh.s22ohjelmistoprojekti1.domain;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Cloth {
 
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private Long id;
+	private String name, type, producer, description;
+	private double price;
+	
+	public Cloth() {
+		
+	}
+	
+	public Cloth(Long id, String name, String type, String producer, String description, double price) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.type = type;
+		this.producer = producer;
+		this.description = description;
+		this.price = price;
+	}
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
+	public String getProducer() {
+		return producer;
+	}
+	public void setProducer(String producer) {
+		this.producer = producer;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public double getPrice() {
+		return price;
+	}
+	public void setPrice(double price) {
+		this.price = price;
+	}
+	
+	
 }
