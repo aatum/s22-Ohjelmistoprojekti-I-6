93e34ec72cf0f773cdea9ad47825bcfca25a1d1f
Jani <jani.n@icloud.com>
2022-10-31 10:05:20 +0200

    ClothRepository

---
+++ s22ohjelmistoprojekti1/s22ohjelmistoprojekti1/src/main/java/hh/s22ohjelmistoprojekti1/domain/ClothRepository.java
@@ -0,0 +1,7 @@
+package hh.s22ohjelmistoprojekti1.domain;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface ClothRepository extends CrudRepository<Cloth, Long> {
+
+}
--- s22ohjelmistoprojekti1/s22ohjelmistoprojekti1/src/main/java/hh/s22ohjelmistoprojekti1/web/ClothController.java
+++ s22ohjelmistoprojekti1/s22ohjelmistoprojekti1/src/main/java/hh/s22ohjelmistoprojekti1/web/ClothController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import hh.s22ohjelmistoprojekti1.domain.Cloth;
+import hh.s22ohjelmistoprojekti1.domain.ClothRepository;
 
 
 
