bfed482b9dcbf2526cec6ef32d1f84c0693b5b0a
Aatu Marttila <aatumarttila@gmail.com>
2022-11-07 09:13:36 +0200

    j-unit test

--- s22ohjelmistoprojekti1/s22ohjelmistoprojekti1/src/main/java/hh/s22ohjelmistoprojekti1/domain/ClothRepository.java
+++ s22ohjelmistoprojekti1/s22ohjelmistoprojekti1/src/main/java/hh/s22ohjelmistoprojekti1/domain/ClothRepository.java
@@ -1,7 +1,10 @@
 package hh.s22ohjelmistoprojekti1.domain;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 
 public interface ClothRepository extends CrudRepository<Cloth, Long> {
 
+	List<Cloth> findByName(String name);
 }
