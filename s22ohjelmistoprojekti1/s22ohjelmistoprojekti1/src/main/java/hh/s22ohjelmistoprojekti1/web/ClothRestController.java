077ccaae9753f8db0cf784d54b1bd1259aaddb64
Elias <eliashelin98@gmail.com>
2022-12-09 16:18:09 +0200

    vaatteiden addaus ja edittaus sekä producerien addays ja edittaus ja poisto

--- s22ohjelmistoprojekti1/s22ohjelmistoprojekti1/src/main/java/hh/s22ohjelmistoprojekti1/web/ClothRestController.java
+++ s22ohjelmistoprojekti1/s22ohjelmistoprojekti1/src/main/java/hh/s22ohjelmistoprojekti1/web/ClothRestController.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -12,6 +13,8 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -38,6 +41,21 @@ public class ClothRestController {
 		clothRepository.deleteById(id);
 	}
 	
+	@DeleteMapping("api/producers/{id}")
+	void deleteProducer(@PathVariable Long id) {
+		producerRepository.deleteById(id);
+	}
+	
+	@PutMapping("api/cloths/{id}")
+	void editCloth(@PathVariable Long id, @RequestBody Cloth cloth) {
+		clothRepository.save(cloth);
+	}
+	
+	@PutMapping("api/producers/{id}")
+	void editProducer(@PathVariable Long id, @RequestBody Producer producer) {
+		producerRepository.save(producer);
+	}
+	
 	@RequestMapping(value = "api/cloths")
 	public @ResponseBody List<Cloth> getClothes() {
 		return (List<Cloth>) clothRepository.findAll();
@@ -46,5 +64,15 @@ public class ClothRestController {
 	@RequestMapping(value = "api/producers")
 	public @ResponseBody List<Producer> getProducers() {
 		return (List<Producer>) producerRepository.findAll();
+	}
+	
+	@PostMapping(value = "/api/cloths")
+	public void saveCloth(@RequestBody Cloth cloth) {
+		clothRepository.save(cloth);
+	}
+	
+	@PostMapping(value = "/api/producers")
+	public void saveProducer(@RequestBody Producer producer) {
+		producerRepository.save(producer);
 	}
 }
